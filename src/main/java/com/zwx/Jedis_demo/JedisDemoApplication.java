cecb5fb1e890e002b05bf45a01fff7f8ef7265b6
zhangwenxuan <1065344157@qq.com>
2018-11-07 16:21:31 +0800

    Initial commit

---
+++ src/main/java/com/zwx/Jedis_demo/JedisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zwx.Jedis_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JedisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JedisDemoApplication.class, args);
+		System.out.println("213123");
+	}
+}
